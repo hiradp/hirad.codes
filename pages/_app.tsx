b518e073e50c6bbcc91e30e7e41d80d3a57e4c2a
Hirad Pourtahmasbi <git@hirad.codes>
2021-12-04 21:32:53 -0500

    fix: apply dark/light mode to the whole webpage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ReactElement } from 'react';
 
+import '../styles/globals.css';
+
 function MyApp({ Component, pageProps }: AppProps): ReactElement {
-  return <Component {...pageProps} />;
+  return <div className='markdown-body'><Component {...pageProps} /></div>;
 }
 export default MyApp;
